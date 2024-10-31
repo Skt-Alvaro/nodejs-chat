de8385187663214eefadbd1242595ff0176585be
Alvaro <alvaropilarte123@gmail.com>
2024-10-31 11:34:46 -0600

    connection url changed to .env variable

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 
 async function connectDB() {
   try {
-    await connect("mongodb://user:user@localhost:27017/chat?authSource=admin");
+    await connect(process.env.MONGODB_URL);
     console.log("Conectado correctamente");
   } catch (error) {
     console.log(error);
